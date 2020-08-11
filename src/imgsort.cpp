191469094afc9fee3f2696d3a8a09e42ec4dbd1c
GrimReaperFloof <GrimReaperFloof@users.noreply.github.com>
2020-08-11 02:15:24 +0200

    Code cleanup: use templates to avoid spaghetti code

--- src/imgsort.cpp
+++ src/imgsort.cpp
@@ -3,8 +3,33 @@
 #include <opencv2/highgui/highgui.hpp>
 #include <iostream>
 
-void SortImage3b(cv::Mat &I);
-void SortImage4b(cv::Mat &I);
+template <uchar Channels>
+void SortImage(cv::Mat &I)
+{
+	using VecType = cv::Vec<uchar, Channels>;
+
+	for(int row = 0; row < I.rows; ++row)
+	{
+		VecType *pixelsInRow = new VecType[I.cols];
+		for(int col = 0; col < I.cols; ++col)
+		{
+			pixelsInRow[col] = I.at<VecType>(row, col);
+		}
+
+		for(int channel = I.channels()-1; channel >= 0; --channel)
+		{
+			std::stable_sort(pixelsInRow, pixelsInRow + I.cols, [&channel](VecType a, VecType b) {
+				return a[channel] < b[channel];
+			});
+		}
+
+		for(int col = 0; col < I.cols; ++col)
+		{
+			I.at<VecType>(row, col) = pixelsInRow[col];
+		}
+		delete[] pixelsInRow;
+	}
+}
 
 int main(int argc, char* argv[])
 {
@@ -12,9 +37,11 @@ int main(int argc, char* argv[])
 	//cv::cvtColor(image, image, cv::COLOR_BGR2HSV);
 	switch (image.channels())
 	{
-		case 3: SortImage3b(image);
+		case 1: SortImage<1>(image);
 			break;
-		case 4: SortImage4b(image);
+		case 3: SortImage<3>(image);
+			break;
+		case 4: SortImage<4>(image);
 			break;
 		default: std::cout << std::string("The input image has ") + std::to_string(image.channels()) + std::string(" channels, which is not supported." + std::string("\n"));
 			return(1);
@@ -25,52 +52,3 @@ int main(int argc, char* argv[])
 	return 0;
 }
 
-void SortImage3b(cv::Mat &I)
-{
-	for(int row = 0; row < I.rows; ++row)
-	{
-		cv::Vec3b *pixelsInRow = new cv::Vec3b[I.cols];
-		for(int col = 0; col < I.cols; ++col)
-		{
-			pixelsInRow[col] = I.at<cv::Vec3b>(row, col);
-		}
-
-		for(int channel = I.channels()-1; channel >= 0; --channel)
-		{
-			std::stable_sort(pixelsInRow, pixelsInRow + I.cols, [&channel](cv::Vec3b a, cv::Vec3b b) {
-				return a[channel] < b[channel];
-			});
-		}
-
-		for(int col = 0; col < I.cols; ++col)
-		{
-			I.at<cv::Vec3b>(row, col) = pixelsInRow[col];
-		}
-		delete[] pixelsInRow;
-	}
-}
-
-void SortImage4b(cv::Mat &I)
-{
-	for(int row = 0; row < I.rows; ++row)
-	{
-		cv::Vec4b *pixelsInRow = new cv::Vec4b[I.cols];
-		for(int col = 0; col < I.cols; ++col)
-		{
-			pixelsInRow[col] = I.at<cv::Vec4b>(row, col);
-		}
-
-		for(int channel = I.channels()-1; channel >= 0; --channel)
-		{
-			std::stable_sort(pixelsInRow, pixelsInRow + I.cols, [&channel](cv::Vec4b a, cv::Vec4b b) {
-				return a[channel] < b[channel];
-			});
-		}
-
-		for(int col = 0; col < I.cols; ++col)
-		{
-			I.at<cv::Vec4b>(row, col) = pixelsInRow[col];
-		}
-		delete[] pixelsInRow;
-	}
-}
